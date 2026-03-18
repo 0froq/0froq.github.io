1307446c154af63309819fa7dc2508a64861f4ad
0froq <sayhola@froq.me>
2026-03-18 15:48:35 +0800

    refactor: LinkUnderline to use FloatWindow for tooltips
    Extract tooltip logic into FloatWindow component for better
    reusability and positioning. Add slot support for custom tooltip
    content and props for mouse follow/placement options.

--- docs/.vitepress/theme/components/LinkUnderline.vue
+++ docs/.vitepress/theme/components/LinkUnderline.vue
@@ -1,71 +1,64 @@
 <script setup lang="ts">
-import { useEventListener } from '@vueuse/core'
-import { nextTick, onMounted, reactive, ref } from 'vue'
+import { useAttrs, computed, onMounted, ref } from 'vue'
 import { renderMdInline } from '../../utils/renderMdInline'
+import FloatWindow from './FloatWindow.vue'
 
-const props = defineProps<{
-  href: string
-  text: string
-  tooltipAddons?: string[]
-  tooltipText?: string
-  vanilla?: boolean
-}>()
-
-const showTooltip = ref(false)
-const tooltipRef = ref<HTMLElement | null>(null)
-const mouseX = ref(0)
-const mouseY = ref(0)
-const tooltipStyle = reactive({
-  left: '0px',
-  top: '0px',
+defineOptions({
+  inheritAttrs: false,
 })
 
-async function updateTooltipPosition(e: MouseEvent) {
-  if (!props.tooltipText) {
-    return
-  }
+const attrs = useAttrs()
 
-  mouseX.value = e.clientX
-  mouseY.value = e.clientY
+const props = withDefaults(defineProps<Props>(), {
+  followMouse: true,
+  placement: 'bottom',
+  offset: 8,
+})
 
-  if (showTooltip.value) {
-    await nextTick()
-    if (tooltipRef.value) {
-      const tooltipWidth = tooltipRef.value.offsetWidth
-      const tooltipHeight = tooltipRef.value.offsetHeight
-      const viewportWidth = window.innerWidth
-      const viewportHeight = window.innerHeight
+interface Props {
+  href: string
+  text: string
+  tooltipText?: string
+  vanilla?: boolean
+  followMouse?: boolean
+  placement?: 'bottom' | 'top' | 'left' | 'right'
+  offset?: number
+}
 
-      let newLeft = mouseX.value + 10
-      let newTop = mouseY.value + 10
+const showTooltip = ref(false)
+const triggerRef = ref<HTMLElement | null>(null)
+const floatWindowRef = ref<InstanceType<typeof FloatWindow> | null>(null)
 
-      // Check if tooltip goes off right edge
-      if (newLeft + tooltipWidth > viewportWidth) {
-        newLeft = mouseX.value - tooltipWidth - 10
-      }
+const linkContent = computed(() => renderMdInline(props.text))
+const tooltipContent = computed(() =>
+  props.tooltipText ? renderMdInline(props.tooltipText) : '',
+)
 
-      // Check if tooltip goes off bottom edge
-      if (newTop + tooltipHeight > viewportHeight) {
-        newTop = mouseY.value - tooltipHeight - 10
-      }
+function handleMouseEnter() {
+  showTooltip.value = true
+}
 
-      tooltipStyle.left = `${newLeft}px`
-      tooltipStyle.top = `${newTop}px`
-    }
+function handleMouseLeave() {
+  showTooltip.value = false
+}
+
+function handleMouseMove(e: MouseEvent) {
+  if (props.followMouse) {
+    floatWindowRef.value?.updateMousePosition(e)
   }
 }
 
-onMounted(() => {
-  useEventListener(window, ['resize', 'scroll'], () => showTooltip.value = false)
-})
+console.warn(attrs['un-text'])
 </script>
 
 <template>
   <div
     v-if="vanilla"
-    @mouseenter="showTooltip = true"
-    @mouseleave="showTooltip = false"
-    @mousemove="showTooltip = true; updateTooltipPosition($event)"
+    ref="triggerRef"
+    class="link-underline-wrapper"
+    @mouseenter="handleMouseEnter"
+    @mouseleave="handleMouseLeave"
+    @mousemove="handleMouseMove"
   >
     <a
       un-transition-colors
@@ -79,42 +72,13 @@ onMounted(() => {
       :href="href"
       class="markdown-rendered"
       v-bind="$attrs"
-      v-html="renderMdInline(text)"
+      v-html="linkContent"
     />
-    <div
-      v-if="showTooltip && tooltipText"
-      ref="tooltipRef"
-      class="tooltip"
-      :style="tooltipStyle"
-      un-bg="neutral-200 dark:neutral-800"
-      un-text="neutral-800 dark:neutral-200"
-      un-fixed
-      un-rounded-sm
-      un-whitespace-nowrap
-      un-text-align-start
-      un-z-50
-      un-py-2
-      un-px-4
-      un-shadow-lg
-    >
-      <div
-        un-flex="~ col"
-        un-max-w="300px"
-      >
-        <div
-          class="markdown-rendered"
-          un-break-words
-          un-whitespace-normal
-          v-html="renderMdInline(tooltipText)"
-        />
-        <slot
-          name="tooltipAddons"
-        />
-      </div>
-    </div>
   </div>
-  <div
+
+  <span
     v-else
+    ref="triggerRef"
     un-after="content-empty bg-neutral-400 dark:bg-neutral-600 w-full h-1px absolute bottom-0.5 left-0 z-0"
     un-inline-block
     un-duration-400
@@ -131,9 +95,10 @@ onMounted(() => {
     un-hover-before-w-full
     un-before-transition-width
     un-before-content-empty
-    @mouseenter="showTooltip = true"
-    @mouseleave="showTooltip = false"
-    @mousemove="showTooltip = true; updateTooltipPosition($event)"
+    un-before-bg="[--decoration-color]"
+    @mouseenter="handleMouseEnter"
+    @mouseleave="handleMouseLeave"
+    @mousemove="handleMouseMove"
   >
     <a
       un-block
@@ -143,21 +108,25 @@ onMounted(() => {
       un-overflow-hidden
       :href="href"
       class="markdown-rendered"
-      v-html="renderMdInline(text)"
+      v-html="linkContent"
     />
+  </span>
+
+  <FloatWindow
+    v-if="tooltipText || $slots.tooltip"
+    ref="floatWindowRef"
+    v-model:visible="showTooltip"
+    :trigger-ref="triggerRef"
+    :follow-mouse="followMouse"
+    :placement="placement"
+    :offset="offset"
+  >
     <div
-      v-if="showTooltip && tooltipText"
-      ref="tooltipRef"
-      class="tooltip"
-      :style="tooltipStyle"
+      v-if="tooltipText"
       un-bg="neutral-200 dark:neutral-800"
       un-text="neutral-800 dark:neutral-200"
-      un-fixed
       un-rounded-sm
-      un-whitespace-nowrap
       un-text-align-start
-      un-text-base
-      un-z-50
       un-py-2
       un-px-4
       un-shadow-lg
@@ -170,12 +139,25 @@ onMounted(() => {
           class="markdown-rendered"
           un-break-words
           un-whitespace-normal
-          v-html="renderMdInline(tooltipText)"
+          v-html="tooltipContent"
         />
-        <slot
-          name="tooltipAddons"
-        />
+        <slot name="tooltipAddons" />
       </div>
     </div>
-  </div>
+
+    <div
+      v-else
+      un-bg="neutral-200 dark:neutral-800"
+      un-text="neutral-800 dark:neutral-200"
+      un-rounded-sm
+      un-text-align-start
+      un-py-2
+      un-px-4
+      un-shadow-lg
+    >
+      <slot
+        name="tooltip"
+      />
+    </div>
+  </FloatWindow>
 </template>
