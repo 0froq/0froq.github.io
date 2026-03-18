0b69c434f9b964bb6196756128bbf2fa31ac4e42
0froq <sayhola@froq.me>
2026-03-18 15:49:04 +0800

    refactor: PageContent to use new ContentIndexXXX components
    Replace monolithic ContentIndex with route-specific index
    components for better code organization and maintainability.

--- docs/.vitepress/theme/components/PageContent.vue
+++ docs/.vitepress/theme/components/PageContent.vue
@@ -5,23 +5,46 @@ import ContentContact from './ContentContact.vue'
 import ContentCorpusLayer from './ContentCorpusLayer.vue'
 import ContentDashboardHints from './ContentDashboardHints.vue'
 import ContentDashboardIntents from './ContentDashboardIntents.vue'
-import ContentIndex from './ContentIndex.vue'
 import ContentNav from './ContentNav.vue'
 import ContentNotFound from './ContentNotFound.vue'
 import ContentTags from './ContentTags.vue'
+import ContentTopIndex from './ContentTopIndex.vue'
+
+import ContentIndexCorpus from './ContentIndexCorpus.vue'
+import ContentIndexDashboard from './ContentIndexDashboard.vue'
+import ContentIndexGlobal from './ContentIndexGlobal.vue'
+import ContentIndexPosts from './ContentIndexPosts.vue'
+import ContentIndexTags from './ContentIndexTags.vue'
 
 const { page } = useData()
 const route = useRoute()
 
-      // v-else-if="route.path.match(/^\/corpus\/[0-5]00_[a-z]+\/$/)"
+// v-else-if="route.path.match(/^\/corpus\/[0-5]00_[a-z]+\/$/)"
 </script>
 
 <template>
   <un-page-content
     un-min-h="[calc(100vh-15rem)]"
   >
-    <ContentNav />
-    <ContentIndex v-if="page.frontmatter.index" />
+    <!-- <ContentTopIndex v-if="page.frontmatter.index" /> -->
+    <template
+      v-if="page.frontmatter.index"
+    >
+      <ContentIndexGlobal v-if="['/', '/en/'].includes(route.path)" />
+      <ContentIndexCorpus
+        v-else-if="['/corpus/', '/corpus/en/'].includes(route.path)"
+      />
+      <ContentIndexPosts
+        v-else-if="['/posts/', '/posts/en/'].includes(route.path)"
+      />
+      <ContentIndexDashboard
+        v-else-if="['/dashboard/', '/dashboard/en/'].includes(route.path)"
+      />
+      <ContentIndexTags
+        v-else-if="['/tags/', '/tags/en/'].includes(route.path)"
+      />
+      <ContentNotFound v-else />
+    </template>
     <ContentCorpusLayer
       v-else-if="route.path.startsWith('/corpus/') && page.frontmatter.locale"
     />
