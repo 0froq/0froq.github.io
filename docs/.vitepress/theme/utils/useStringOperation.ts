9f21541dcf19935dcb4b88b4678574142d9b24e2
0froq <sayhola@froq.me>
2026-04-02 15:41:51 +0800

    feat(utils): add useStringOperation composable

---
+++ docs/.vitepress/theme/utils/useStringOperation.ts
@@ -0,0 +1,11 @@
+export function useStringOperation() {
+  const capitalize = (str: string) => {
+    if (str.length === 0)
+      return str
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  return {
+    capitalize,
+  }
+}
